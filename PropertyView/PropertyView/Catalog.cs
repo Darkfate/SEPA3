07fcbc27e24377795f8de1aaa97b4f8dbaaf59b5
= <=>
2012-10-27 16:20:35 +1100

    Create new project and added some classes in
    Added Catalog, Property and Video classes.

---
+++ PropertyView/PropertyView/Catalog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyView
+{
+    public class Catalog
+    {
+        private Property[] properties;
+
+        public Property[] search(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
---
+++ PropertyView/PropertyView/Properties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PropertyView
+{
+    public class Property
+    {
+        private int propertyID;
+
+        public Video getVideo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
