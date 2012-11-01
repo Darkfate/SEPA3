ac66e264d91263ba69956dfcf795852522669eac
= <=>
2012-11-01 22:50:35 +1100

    Added video playre class

---
+++ PropertyView/PropertyView/VideoPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyView
+{
+    class VideoPlayer
+    {
+        private Video[] videos;
+
+        public void Play()
+        {
+            //play video
+        }
+    }
+}
