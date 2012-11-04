ef29f189746eaa936cef93a9729d9a2f8369046a
swatcop3 <swat_cop3@yahoo.com>
2012-11-04 23:11:56 +1100

    add some more function

--- PropertyView/PropertyView/Catalog.cs
+++ PropertyView/PropertyView/Catalog.cs
@@ -19,6 +19,27 @@ namespace PropertyView
         {
             throw new NotImplementedException();
         }
+        public void addProperty()
+        {
+            Console.WriteLine("Please enter Property Name");
+            string propertyName = Console.ReadLine();
+            
+            Console.WriteLine("Please enter Property Address");
+            string propertyAddress= Console.ReadLine();
+
+            int propertyID = properties.Length + 1;
+
+
+        }
+        public void displayProperty()
+        {
+            for(int i=0;i<properties.Length; i++)
+            {
+                Console.WriteLine(properties[i].getPropertyID());
+                Console.WriteLine(properties[i].getPropertyName());
+                Console.WriteLine(properties[i].getPropertyAddress());
+            }
+        }
     }
 }
 
--- PropertyView/PropertyView/Properties.cs
+++ PropertyView/PropertyView/Properties.cs
@@ -8,10 +8,34 @@ namespace PropertyView
     public class Property
     {
         private int propertyID;
+        private string propertyName;
+        private string propertyAddress;
 
+        public Property(int propertyID, string propertyName, string propertyAddress)
+        {
+            this.propertyID = propertyID;
+            this.propertyName = propertyName;
+            this.propertyAddress = propertyAddress;
+
+        }
         public Video getVideo()
         {
             throw new NotImplementedException();
+        }
+
+        public int getPropertyID()
+        {
+            return propertyID;
+        }
+
+        public string getPropertyName()
+        {
+            return propertyName;
+        }
+
+        public string getPropertyAddress()
+        {
+            return propertyAddress;
         }
     }
 }
