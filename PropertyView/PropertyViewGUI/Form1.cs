3a64b32fe4bf04aa5e22aba56562cb0f60311bb0
= <=>
2012-11-07 22:19:21 +1100

    Added propery view UI

---
+++ PropertyView/PropertyViewGUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PropertyView;
+
+namespace PropertyViewGUI
+{
+    public partial class CatalogView : Form
+    {
+        private Catalog catalog;
+        private List<Property> propertyList;
+
+        public CatalogView()
+        {
+            InitializeComponent();
+            catalog = new Catalog();
+            propertyList = new List<Property>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            propertyList = catalog.search(textBox1.Text).ToList();
+        }
+    }
+}
